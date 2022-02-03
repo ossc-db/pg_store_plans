cb6bda4dab991911cd6bc1faaca993f55c4bf2ac
Kyotaro Horiguchi <horikyota.ntt@gmail.com>
2022-02-03 22:50:38 +0900

    Add forgot upgrade sql file
    Add upgrade SQL script forgot to be included in the last commits.

---
+++ pg_store_plans--1.6--1.6.1.sql
@@ -0,0 +1,4 @@
+/* pg_store_plans/pg_store_plans--1.6--1.6.1.sql */
+
+-- complain if script is sourced in psql, rather than via CREATE EXTENSION
+\echo Use "CREATE EXTENSION pg_store_plans" to load this file. \quit
